e42897bf8f23c36e72f4f7599201b3691b1c0eb7
IN2DM3B_10 <IN2DM3B_10@H015112.NET>
2019-11-27 10:01:49 +0100

    Imagenes e inicio del tablero

---
+++ src/controlador/Principal.java
@@ -0,0 +1,14 @@
+package controlador;
+
+import vista.VentanaPpal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		VentanaPpal juego = new VentanaPpal();
+		
+		juego.setVisible(true);
+
+	}
+
+}
