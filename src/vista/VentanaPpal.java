990f1242816dd3bfe6be96e63eb6b85cda2d59c4
IN2DM3B_10 <IN2DM3B_10@H015112.NET>
2019-11-27 10:04:44 +0100

    Cambios menores

--- src/vista/VentanaPpal.java
+++ src/vista/VentanaPpal.java
@@ -1,23 +1,20 @@
 package vista;
 
-import java.awt.BorderLayout;
 import java.awt.Button;
-import java.awt.EventQueue;
-import java.awt.Label;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.GridLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
 
 public class VentanaPpal extends JFrame {
 
 	private JPanel contentPane;
-	public Button[][] casillas;
-	public Label[][] tablero;
+	public JButton[][] casillas;
+	public JLabel[][] tablero;
 
 	public VentanaPpal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,15 +24,33 @@ public class VentanaPpal extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(0, 0, 30, 23);
-		btnNewButton.setIcon(new ImageIcon(VentanaPpal.class.getResource("/imagenes/t-3.png")));
-		contentPane.add(btnNewButton);
+		JPanel panel = new JPanel();
+		panel.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		panel.setBounds(0, 0, 208, 48);
+		contentPane.add(panel);
+		panel.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setIcon(new ImageIcon(VentanaPpal.class.getResource("/javax/swing/plaf/basic/icons/JavaCup16.png")));
-		lblNewLabel.setBounds(86, 76, 46, 14);
-		contentPane.add(lblNewLabel);
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel_1.setBounds(8, 8, 192, 32);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(10, 0, 13, 23);
+		lblNewLabel.setIcon(new ImageIcon("C:\\workspace-accesodatos\\BuscaMinas\\imagenes\\d0.png"));
+		panel_1.add(lblNewLabel);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		panel_2.setBounds(0, 48, 208, 213);
+		contentPane.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel_3.setBounds(10, 11, 188, 191);
+		panel_2.add(panel_3);
 		inicializarCasillas();
 		inicializarTablero();
 	}
